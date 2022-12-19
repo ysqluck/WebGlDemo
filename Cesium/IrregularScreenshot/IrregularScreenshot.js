b9828b0161b8c05234c8bd64a051518a070fc636
yunchan <2183922902@qq.com>
2022-12-19 21:03:36 +0800

    cesium截图功能完成

--- Cesium/IrregularScreenshot/IrregularScreenshot.js
+++ Cesium/IrregularScreenshot/IrregularScreenshot.js
@@ -1,15 +1,11 @@
 // 实现思路
 // 在cesium1.9.5之后 可以通过viewer.canvas拿到canvas对象
 // 通过操作canvas对象方式截图
-function getCanvas(viewer) {
-  viewer.render();
-  // 重新生成一个canvas的节点，用来复制cesium的canvas
-  let canvasDom = document.createElement("canvas");
-  canvasDom.width = viewer.canvas.width;
-  canvasDom.height = viewer.canvas.height;
-  return canvasDom.getContext("2d");
-}
-function canvasDrawLine(ctx, arr) {
+let canvasDom = document.createElement("canvas");
+canvasDom.width = viewer.canvas.width;
+canvasDom.height = viewer.canvas.height;
+function canvasDrawLine(arr) {
+  let ctx = canvasDom.getContext('2d')
   ctx.strokeStyle = "black";
   ctx.lineWidth = 1;
   ctx.moveTo(arr[0][0], arr[0][1]);
@@ -17,4 +13,33 @@ function canvasDrawLine(ctx, arr) {
     ctx.lineTo(arr[i][0], arr[i][1]);
   }
   ctx.closePath();
+  ctx.stroke();
+  ctx.fillStyle = "white";
+  ctx.fill();
+  ctx.beginPath();
+  ctx.globalCompositeOperation = "source-out";
+  ctx.rect(0, 0, window.innerWidth, window.innerHeight);
+  // ctx.fillStyle = "grey";
+  // ctx.fill();
+  viewer.render();
+  ctx.drawImage(viewer.canvas, 0, 0, window.innerWidth, window.innerHeight); //这个时候拿到了当前视口宽度的截图
+  ownLoadImgBase64(canvasDom.toDataURL())
+}
+function ownLoadImgBase64(base64) {
+  if (window.navigator.msSaveOrOpenBlob) {
+    var bstr = atob(base64.split(",")[1]);
+    var n = bstr.length;
+    var u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    var blob = new Blob([u8arr]);
+    window.navigator.msSaveOrOpenBlob(blob, "chart-download" + "." + "png");
+  } else {
+    // 这里就按照chrome等新版浏览器来处理
+    const a = document.createElement("a");
+    a.href = base64;
+    a.setAttribute("download", "chart-download");
+    a.click();
+  }
 }
